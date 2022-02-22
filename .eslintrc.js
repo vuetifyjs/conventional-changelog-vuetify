133ca71396999dc078c3ad19035a675dfb3bad83
Kael <kaelwd@gmail.com>
2022-02-23 00:44:06 +1100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: 'standard',
+
+  rules: {
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'only-multiline',
+    }],
+  },
+}
